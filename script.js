033cd19a990cedfe6ae7603fb1b0a642aae0e8f6
Jaesunee <jach7279@colorado.edu>
2021-10-24 19:02:36 -0600

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function displayChampion() {
   var championInfo = document.getElementById('championInfo');
-  var championName = document.getElementById('championName');
+  var championName = document.getElementById('championName').value;
 
-  championInfo.innerHTML = 'The Champion is ' + championName.value;
+  championInfo.innerHTML = 'The Champion is ' + championName;
 }
